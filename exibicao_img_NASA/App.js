290f192994bea4833c1bd25eb63d055e47dc5c11
Caio Lima <caiolimacruz@gmail.com>
2025-11-22 15:50:23 -0300

    feat(P2): Busca funciona pelo back

--- exibicao_img_NASA/App.js
+++ exibicao_img_NASA/App.js
@@ -1,13 +1,12 @@
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import { FontAwesome6, FontAwesome5 } from '@expo/vector-icons';
 import { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, Pressable, TextInput, Linking } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Pressable, TextInput, Linking, Image } from 'react-native';
 
 export default function App() {
 
-  const [foto, setFoto] = useState('')
+  const [busca, setBusca] = useState('')
   const [fotos, setFotos] = useState([])
-  const [anos, setAnos] = useState([])
 
 const abrirLinkedIn1 = () => {
   Linking.openURL('https://www.linkedin.com/in/caio-lima-da-cruz-338850337/')
@@ -21,32 +20,76 @@ const abrirLinkedIn3 = () => {
   Linking.openURL('www.linkedin.com/in/caio-lima-da-cruz-338850337')
 }
 
+//funções esperadas: buscar, buscarAno
+
+const buscar = async () =>{
+  if(busca === '') return
+   const resposta = await fetch(`http://localhost:3000/search?termo=${busca}`)
+    const dados = await resposta.json()
+
+    const resultados = dados.items || []
+    setFotos(resultados)
+}
+
+const buscarAno = async (ano) => {
+   const buscarAno = async (ano) => {
+    const resposta = await fetch(`http://localhost:3000/search-year?ano=${ano}`)
+    const dados = await resposta.json()
+
+    const resultados = dados.items || []
+    setFotos(resultados)
+   }
+} 
+
   return (
     <View style={styles.container}>
-      {/* <FlatList>
-        <Text>Fotos do Dia</Text>
-        //<ListHeaderComponent> para as fotos do dia
-      </FlatList> */}
+      {/* <FlatList ListHeaderComponent={foto}/> */}
       <TextInput
         style={styles.input}
-        value={foto}
-        onChangeText={setFoto}
+        value={busca}
+        onChangeText={setBusca}
         placeholder='Digite o que deseja buscar(exemplos: moon, earth etc)'
       />
 
 
-      <Pressable style={styles.botao}>
-        {/* onPress={busca} */}
+      <Pressable style={styles.botao} onPress={buscar}>
         <Text style={{ color: 'white' }}>Ao infinito, e além...</Text>
       </Pressable>
 
       <View style={styles.containerAno}>
-        {['2020', '2021', '2022', '2023', '2024'].map(ano => (
-          <Pressable key={ano} style={styles.botaoAno}>
+        {['2020', '2021', '2022', '2023', '2024'].map((ano) => (
+          <Pressable 
+            key={ano} 
+            style={styles.botaoAno} 
+            onPress={()=>buscarAno(ano)}
+            >
             <Text style={{ color: 'white' }}>{ano}</Text>
           </Pressable>
         ))}
       </View>
+
+      <FlatList 
+          data={fotos}
+          style={{ marginTop: 20, width: "100%" }}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => {
+          const imagem = item.links ? item.links[0].href : null
+
+          if (!imagem) return null
+
+          return (
+            <View style={styles.fotos}>
+              <Image
+                source={{ uri: imagem }}
+                style={{ width: 300, height: 300, borderRadius: 10 }}
+              />
+              <Text style={styles.titulo}>{item.data[0].title}</Text>
+            </View>
+          )
+        }}
+      />
+
+
       <Pressable style={styles.botao}>
         <Text style={{ color: 'white' }}>2025</Text>
       </Pressable>
@@ -130,5 +173,19 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: 0,
 
+  },
+
+    fotos: {
+    marginBottom: 40,
+    alignItems: 'center'
+  },
+
+  
+  titulo: {
+    color: "white",
+    marginTop: 10,
+    fontSize: 16,
+    width: 300,
+    textAlign: "center"
   },
 });
---
+++ exibicao_img_NASA/Back/index.js
@@ -0,0 +1,73 @@
+const express = require("express")
+const axios = require("axios")
+const cors = require("cors")
+require("dotenv").config()
+
+const app = express()
+app.use(cors())
+
+app.get('/search', async (req, res) => {
+    const termo = req.query.termo
+
+    if (!termo) {
+        return res.status(400).json({ erro: "O parâmetro 'termo' é obrigatório" })
+    }
+
+    const nasa = axios.create({
+        baseURL: 'https://images-api.nasa.gov/'
+    })
+
+    const result = await nasa.get('/search', {
+        params: {
+            q: termo,
+            media_type: 'image',
+        }
+    })
+
+    res.json({ items: result.data.collection.items })
+})
+
+
+app.get('/search-year', async (req, res) => {
+    const ano = req.query.ano;
+
+    if (!ano) {
+        return res.status(400).json({ erro: "O parâmetro 'ano' é obrigatório" })
+    }
+
+    const nasa = axios.create({
+        baseURL: 'https://images-api.nasa.gov/'
+    })
+
+    const result = await nasa.get('/search', {
+        params: {
+            year_start: ano,
+            year_end: ano,
+            media_type: 'image'
+        }
+    });
+
+    res.json({ items: result.data.collection.items })
+})
+
+
+
+app.get('/apod', async (req, res) => {
+    const nasa = axios.create({
+        baseURL: 'https://api.nasa.gov/planetary/'
+    })
+
+    const result = await nasa.get('/apod', {
+        params: {
+            api_key: process.env.NASA_KEY
+        }
+    })
+    
+    res.json(result.data)
+})
+
+
+
+const port = 3000
+app.listen(port, () => {console.log(`Back. Porta ${port}`)})
+
